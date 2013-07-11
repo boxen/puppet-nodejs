c09142b09bc3ab893f735c0e341fb028445ce3a1
Jozef Izso <jozef.izso@gmail.com>
2013-07-11 13:40:24 +0200

    Updated nodejs::v0_10 to use nodejs 0.10.13

--- spec/classes/nodejs__v0_10_spec.rb
+++ spec/classes/nodejs__v0_10_spec.rb
@@ -4,11 +4,11 @@ describe "nodejs::v0_10" do
   let(:facts) { default_test_facts }
 
   it do
-    should include_class("nodejs::v0_10_7")
+    should include_class("nodejs::v0_10_13")
 
     should contain_file("/test/boxen/nodenv/versions/v0.10").with({
       :ensure => "link",
-      :target => "/test/boxen/nodenv/versions/v0.10.7"
+      :target => "/test/boxen/nodenv/versions/v0.10.13"
     })
   end
 end
