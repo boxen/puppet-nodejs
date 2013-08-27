4871e4b7c05ce8aec55bd9e43121c73f67b17d14
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2013-08-26 22:56:09 -0300

    Make sure all nvm executables are removed

--- spec/classes/nodejs__nvm_spec.rb
+++ spec/classes/nodejs__nvm_spec.rb
@@ -7,6 +7,10 @@ describe "nodejs::nvm" do
     should include_class("boxen::config")
 
     should contain_file("/test/boxen/env.d/nvm.sh").with_ensure("absent")
+    should contain_file("/test/boxen/bin/boxen-nvm-exec").with_ensure("absent")
+    should contain_file("/test/boxen/bin/node").with_ensure("absent")
+    should contain_file("/test/boxen/bin/node-waf").with_ensure("absent")
+    should contain_file("/test/boxen/bin/npm").with_ensure("absent")
 
     should contain_exec("purge nvm").with({
       :command => "rm -rf /test/boxen/nvm",
@@ -21,6 +25,10 @@ describe "nodejs::nvm" do
       should_not include_class("boxen::config")
 
       should_not contain_file("/test/boxen/env.d/nvm.sh")
+      should_not contain_file("/test/boxen/bin/boxen-nvm-exec")
+      should_not contain_file("/test/boxen/bin/node")
+      should_not contain_file("/test/boxen/bin/node-waf")
+      should_not contain_file("/test/boxen/bin/npm")
     end
   end
 end
